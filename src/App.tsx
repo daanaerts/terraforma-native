5f7c4f1ee76ac353712cecba9a47143c820fb780
Daan Aerts <daanaerts@Daans-MacBook-Pro.local>
2018-11-09 18:11:11 +0100

    added ci

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Container, Button, Text } from "native-base";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up as.js to start working on your app!</Text>
-      </View>
+      <Container>
+        <Button>
+          <Text>Button</Text>
+        </Button>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
